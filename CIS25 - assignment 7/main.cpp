77a6ada7150df39f38fd196dc6986eef5c1c67b6
maya-nachiappan <maya.n102304@gmail.com>
2024-10-14 00:59:23 -0700

    added gitignore file and comments

--- CIS25 - assignment 7/main.cpp
+++ CIS25 - assignment 7/main.cpp
@@ -12,14 +12,14 @@ using namespace std;
 int main()
 {
     const int num=3;
-    string names[num];
+    string names[num]; // creates an array to store 3 names
     cout << "Please enter 3 names: " << endl;
     for (int i=0; i<num; i++)
     {
-        cout << "Name " << (i+1) << ":" << endl;
-        getline(cin, names[i]);
+        cout << "Name " << (i+1) << ":" << endl; // the user is prompted to enter 3 names
+        getline(cin, names[i]); // each name is stored in an array index
     }
-    cout << "The first name in the array is: " << names[0] << endl;
+    cout << "The first name in the array is: " << names[0] << endl; // prints each name in the array in index order
     return 0;
 }
 
